814893f05b341ab8aa75620cfa32f04a6aa94459
Giorgi Dzebisashvili <giorgidzebisashvili4@gmail.com>
2023-07-11 17:41:47 +0400

    finished task 3

--- javascript.js
+++ javascript.js
@@ -15,9 +15,49 @@ function checkWinner(playerSelection, computerSelection){
         (playerSelection == "paper" && computerSelection == "rock") ||
         (playerSelection == "scissors" && computerSelection == "paper")
     ){
-        return "player"
-    } else {
-        return "computer"
+        return "player";
+    } 
+    else {
+        return "computer";
     }
+    }
+
+function playerRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "It's a Tie!";
+    }
+    else if(result == "player"){
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
+
+function getPLayerChoice(){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice = prompt("Rock Paper Scissors");
+        if(choice == null){
+            continue;
+        }
+        const choiceInLower = choice.toLocaleLowerCase();
+        if(options.includes(choiceInLower)){
+            validatedInput =true;
+            return choiceInLower;
+        }
+    }
+}
+
+function game(){
+    console.log("Welocme!")
+    for (let i=0; i<5; i++){
+        const playerSelection = getComputerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playerRound(playerSelection, computerSelection));
+    }
+    console.log("Game Over")
+}
+
+game()

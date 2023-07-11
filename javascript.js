2bf9cc67ddc518c6bc2b380c838f6bac13621341
Giorgi Dzebisashvili <giorgidzebisashvili4@gmail.com>
2023-07-11 14:24:14 +0400

    starting over

--- javascript.js
+++ javascript.js
@@ -7,7 +7,25 @@ function getComputerChoice(x){
         return "Scissors";}
 }
 let result=getComputerChoice(Math.floor(Math.random() * 3));
-alert(result);
 
-playerSelection
-computerSelection
+
+let computerSelection = result;
+let playerSelection = prompt("write down: Rock, Paper or Scissors");
+
+function playRound(playerSelection, computerSelection) {
+    if ("Rock","Rock"){
+        alert ("try again");
+    } else if ("Rock", "Paper") {
+        alert (`you lose! ${computerSelection} beats ${playerSelection}`);
+     } else {
+        alert (`you win! ${playerSelection} beats ${computerSelection}`);
+    }
+  }
+
+
+  let loseOrWin = 
+ alert (`You ${loseOrWin}! ${computerSelection} beats ${playerSelection}`)
+// mnishvneloba rom enichebode
+//ertmanets rom edarebodes da idzleodes pasuxebs
+//mnishvnelobebs yvelas rom agiqvavdes
+// 

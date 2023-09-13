bcd857fb413e24b6c57c0b2792106e503fe633e4
Giorgi Dzebisashvili <giorgidzebisashvili4@gmail.com>
2023-09-13 15:43:40 +0400

    its working

--- javascript.js
+++ javascript.js
@@ -1,72 +1,59 @@
-const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const scoreText = document.querySelector("#scoreText");
-const choiceBtns = document.querySelectorAll(".choiceBtn");
-let player;
-let computer;
-let result;
-let scorePlayer;
-let scoreComputer;
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('input')
 
-choiceBtns.forEach(button => button.addEventListener("click", () => {
-
-    player = button.textContent;
-    computerTurn();
-    checkScore();
-    playerText.textContent = `Player: ${player}`;
-    computerText.textContent = `Computer: ${computer}`;
-    resultText.textContent = checkWinner();
-    scoreText.textContent = `p-c ${scorePlayer}-${scoreComputer}`
-}));
-
-function computerTurn(){
-
-    const randNum = Math.floor(Math.random() * 3) + 1;
-
-    switch(randNum){
-      case 1:
-        computer = "ROCK";
-        break;
-      case 2:
-        computer = "PAPER";
-        break;
-      case 3:
-        computer = "SCISSORS";
-        break;
-    }
-}
-function checkWinner(){
-    if(player == computer){
-      return "Draw!";
-    }
-    else if(computer == "ROCK"){
-      return (player == "PAPER") ? "You Win!" : "You Lose!"
-    }
-    else if(computer == "PAPER"){
-      return (player == "SCISSORS") ? "You Win!" : "You Lose!"
-    }
-    else if(computer == "SCISSORS"){
-      return (player == "ROCK") ? "You Win!" : "You Lose!"
-    }
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
-function checkScore(){
-    if (result === "You Win!"){
-        scorePlayer = 1
-    }else if(result = "You Lose!"){
-        scoreComputer = 1
-    }else{}
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
         
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    }
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (computerScore == 5) {
+            result += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
+        }
     }
 
+    document.getElementById('result').innerHTML = result
+    return
+}
 
-
-
-
-
-
-
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
 
 // console.log('hello world');
 

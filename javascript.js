638b6e3e7ee165e5241b72bc2c27ad73c83f0ad5
Giorgi Dzebisashvili <giorgidzebisashvili4@gmail.com>
2023-09-26 15:34:29 +0400

    player selection F

--- javascript.js
+++ javascript.js
@@ -5,4 +5,19 @@ let getComputerChoice = function(){
     let answer = options [Math.floor(Math.random() * 3)];
     return answer
 }
-getComputerChoice()
+let computerSelection = getComputerChoice()
+console.log(computerSelection)
+
+// player chose return first capitilised 
+let  getPlayerChoice = function(){
+    let text = prompt(" chose ‘Rock’, ‘Paper’ or ‘Scissors’");
+    let capitilised = text.charAt(0).toUpperCase() + text.substr(1,text.length-1)
+    
+    return capitilised
+
+}
+
+let playerSelection = getPlayerChoice()
+console.log(playerSelection)
+
+
